5b60faad3d97fa5ba9a534622b5e2254d3eacf7e
lordidiot <0xlordidiot@gmail.com>
2022-10-09 19:31:21 +0800

    initial work on index

---
+++ components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div
+    class="relative w-full flex flex-row justify-between p-6 items-center"
+  >
+    <div id="header-logo">
+      <NuxtLink to="/">
+        <img src="~/assets/img/logo.png" width="120" alt="PagePal Logo">
+      </NuxtLink>
+    </div>
+    <NavBar />
+  </div> 
+</template>
---
+++ components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="flex flex-row justify-around font-semibold"
+  >
+    <NuxtLink to="/books">
+      <div class="mx-4">
+        Books
+      </div>
+    </NuxtLink>
+    <NuxtLink to="/about">
+      <div class="mx-4">
+        About
+      </div>
+    </NuxtLink>
+  </div> 
+</template>
