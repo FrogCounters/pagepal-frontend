23617a201a4e694e3ff77aefbd87080d649c47e1
lordidiot <0xlordidiot@gmail.com>
2022-10-23 15:08:34 +0800

    Redesign story reading page (#4)
    * Book page padding
    
    * Add emojis and colours

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,6 +29,7 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '@/assets/css/main.scss',
+    '@/assets/css/twemoji-awesome.css',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
