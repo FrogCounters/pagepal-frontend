bbab4eec9287411bde09eae72998fba4fa5cc4d9
Guan Quan <76832850+guanquann@users.noreply.github.com>
2022-10-27 11:33:54 +0800

    add security policy

--- pages/book/_title.vue
+++ pages/book/_title.vue
@@ -15,7 +15,7 @@ export default {
   },
   methods: {
     getBooks() {
-      fetch(`https://pagepal-backend-dev.ap-southeast-1.elasticbeanstalk.com/book/${this.$route.params.title}`)
+      fetch(`http://pagepal-backend-dev.ap-southeast-1.elasticbeanstalk.com/book/${this.$route.params.title}`)
         .then((response) => response.json())
         .then((data) => data[this.$route.params.title])
         .then((data) => (this.book = data))
